fa17e3da36ee07e526914bfacc79410046f68e27
Yuta Fukuchi <fufufu10155@gmail.com>
2020-08-12 15:18:45 +0900

    delete function add

--- src/components/Products/ImageArea.jsx
+++ src/components/Products/ImageArea.jsx
@@ -3,7 +3,7 @@ import { storage } from "../../firebase/index";
 import { makeStyles } from "@material-ui/styles";
 import IconButton from "@material-ui/core/IconButton";
 import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
-import ImagePreview from "./ImagePreview"
+import ImagePreview from "./ImagePreview";
 
 const useStyles = makeStyles({
   icon: {
@@ -15,16 +15,19 @@ const useStyles = makeStyles({
 const ImageArea = (props) => {
   const classes = useStyles();
 
-  const deleteImage = useCallback(async(id) => {
-    const ret = window.confirm('この画像を削除しますか')
-    if(!ret) {
-      return false
-    } else {
-      const newImages = props.images.filter(image => image.id !== id)
-      props.setImages(newImages)
-      return storage.ref('images').child(id).delete()
-    }
-  }, [props.images])
+  const deleteImage = useCallback(
+    async (id) => {
+      const ret = window.confirm("この画像を削除しますか");
+      if (!ret) {
+        return false;
+      } else {
+        const newImages = props.images.filter((image) => image.id !== id);
+        props.setImages(newImages);
+        return storage.ref("images").child(id).delete();
+      }
+    },
+    [props.images]
+  );
 
   const uploadImage = useCallback(
     (event) => {
@@ -45,22 +48,26 @@ const ImageArea = (props) => {
         //Handle successful uploads on complete
         uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
           const newImage = { id: fileName, path: downloadURL };
-          props.setImages((prevState => [...prevState, newImage]));
+          props.setImages((prevState) => [...prevState, newImage]);
         });
       });
     },
     [props.setImages]
   );
 
-
-
   return (
     <div>
-        <div className="p-grid__list-images">
-            {props.images.length > 0 && (
-                props.images.map(image => <ImagePreview delete={deleteImage} id={image.id} path={image.path} key={image.id} /> )
-            )}
-        </div>
+      <div className="p-grid__list-images">
+        {props.images.length > 0 &&
+          props.images.map((image) => (
+            <ImagePreview
+              delete={deleteImage}
+              id={image.id}
+              path={image.path}
+              key={image.id}
+            />
+          ))}
+      </div>
       <div className="u-text-right">
         <span>商品画像を登録する</span>
         <IconButton className={classes.icon}>
