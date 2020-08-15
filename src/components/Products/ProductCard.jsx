50ce4a56367e4069ec757b99570a9ecac6260b23
Yuta Fukuchi <fufufu10155@gmail.com>
2020-08-15 13:54:23 +0900

    実践9　終了

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -63,10 +63,10 @@ const ProductCard = (props) => {
             <CardMedia
                 className={classes.media}
                 image={images[0].path}
-                onClick={() => dispatch(push('/product' + props.id))}
+                onClick={() => dispatch(push('/product/' + props.id))}
             />
             <CardContent className={classes.content}>
-                <div onClick={() => dispatch(push('/product' + props.id))}>
+                <div onClick={() => dispatch(push('/product/' + props.id))}>
                     <Typography color="textSecondary" component="p" >
                         {props.name}
                     </Typography>
